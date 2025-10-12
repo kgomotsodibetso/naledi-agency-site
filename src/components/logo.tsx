3899ea1b98a708d3b54b6ea0ea0815085cf24221
Motso Dibetso <dibetsomotso@gmail.com>
2025-10-12 21:58:55 +0000

    Please enlarge the logo  (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,9 +5,9 @@ const NalediLogo = () => (
   <Image
     src="https://firebasestorage.googleapis.com/v0/b/studio-4298127278-2785f.firebasestorage.app/o/Naledi%20Digital%20Logos%2FNaledi%20Digital%20logo%20(1).png?alt=media&token=59ee09a1-01c6-4eb1-b025-73134d43ff57"
     alt="Naledi Digital Logo"
-    width={175}
-    height={40}
-    className="h-10 w-auto"
+    width={210}
+    height={48}
+    className="h-12 w-auto"
     priority
   />
 );
