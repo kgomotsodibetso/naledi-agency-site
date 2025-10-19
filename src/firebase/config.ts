ee55d9d182a26cf508c6772118a3fe9bef9f9a43
Motso Dibetso <dibetsomotso@gmail.com>
2025-10-19 15:59:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4298127278-2785f",
+  "appId": "1:649717656693:web:6a8c442acccbb08151dc3c",
+  "apiKey": "AIzaSyDtzEoWNE02oTENPWL6GHJWAXz1eqOCeLI",
+  "authDomain": "studio-4298127278-2785f.firebaseapp.com",
+  "measurementId": "G-CMV83WRLPY",
+  "messagingSenderId": "649717656693"
+};
